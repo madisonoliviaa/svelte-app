a3a95a0547d421ea0749d1decfda144489522925
madisonoliviaa <madisonoliviaa8@gmail.com>
2023-06-24 04:19:50 -0400

    header- rare stats

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
 
 <ModeSwitcher />
 <main class="p-4 mx-auto text-center max-w-xl">
-	<h1 class="uppercase text-6xl leading-normal font-thin text-svelte">Hello World!</h1>
+	<h1 class="uppercase text-6xl leading-normal font-thin text-svelte">Rare Stats!</h1>
 	<p class="custom-style mt-[3rem]">
 		Visit the
 		<a href="https://svelte.dev/tutorial" class="text-blue-500 dark:text-blue-300 underline">Svelte tutorial</a>
